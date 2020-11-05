7a582d86a2a4cdbe84dd69dbd4b38304db83afa8
qianz <1685437606@qq.com>
2020-11-06 00:27:01 +0800

    doc:add tips

--- my_server.cpp
+++ my_server.cpp
@@ -3,6 +3,8 @@
 #include <netinet/in.h>
 #include "socket/socket.h"
 
+// 打开浏览器看效果
+// http://127.0.0.1:8080
 int main(int argc, char *argv[]) {
     int listen_fd = listen(":8080");
     if (listen_fd == -1){
